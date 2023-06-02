f7046f7fa9e4f84c2ed557810b126c440b38c265
“Bridgeshantanu” <shantanupadul@gmail.com>
2023-06-02 20:44:12 +0530

    Add: Add code is added

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Linkedlist<int> linkedList = new Linkedlist<int>();
+            linkedList.AddNode(70);
+            linkedList.AddNode(30);
+            linkedList.AddNode(56);
+            linkedList.PrintList();
+
+        }
+    }
+}
